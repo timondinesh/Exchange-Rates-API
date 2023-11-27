72677953e7f103cd9e32e5b13cd28be4988e8a13
jinzu1996 <jinzu1996@gmail.com>
2023-11-27 05:38:31 +0530

    Create Exchange Rates API Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [ratesList, setRatesList] = useState([]);
+  const [base, setBase] = useState("USD");
+
+  useEffect(() => {
+    getRates("USD");
+  }, []);
+
+  const getRates = async (base) => {
+    const res = await axios.get(
+      `https://open.er-api.com/v6/latest/${base}`
+    );
+    const { rates } = res.data;
+
+    const ratesTemp = [];
+    for (const [symbol, rate] of Object.entries(rates)) {
+      ratesTemp.push({ symbol, rate });
+    }
+    setRatesList(ratesTemp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select
+        className="custom-select"
+        value={base}
+        onChange={(e) => {
+          const value = e.target.value;
+          setBase(value);
+          getRates(value);
+        }}
+      >
+        {ratesList.map((d) => (
+          <option value={d.symbol} key={d.symbol}>
+            {d.symbol}
+          </option>
+        ))}
+      </select>
+      <ul className="list-group">
+        {ratesList.map((d) => (
+          <li className="list-group-item" key={d.symbol}>
+            {d.symbol} - {d.rate}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
